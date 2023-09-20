e0a1f29fbde564f9241a68da41ebcfe93ccacc91
qinmingjie <1934394876@qq.com>
2023-09-20 20:25:17 +0800

    feat(public,webpack.config.js): 配置html模板以及全局变量插件，静态子资源复制插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name]_[contenthash:4].js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html')
+    }),
+    new DefinePlugin({
+      TITLE: JSON.stringify('App')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/assets', to: 'assets' }]
+    })
+  ]
 };
